c1efc3d1e94ee18951c833f69ab811065346c13d
miro-19 <96656045+miro-19@users.noreply.github.com>
2022-01-23 11:35:14 +1300

    Update train.py

--- train.py
+++ train.py
@@ -19,27 +19,26 @@ def parse_args():
     parser.add_argument('--batch-size', default=512, type=int,help='Batch size')
     parser.add_argument('--Epochs', default=500, type=int,help='num epochs')
     parser.add_argument('--lr', default=1e-3, type=float,help='Initial learning rate. ' + 'Will be decayed until it\'s 1e-5.')
-    parser.add_argument('--resume_file', default=None, type=str,help='Name of saved model to continue training')
-    parser.add_argument('--suffix', default='', type=str,help='Optional descriptive suffix for model')
+    parser.add_argument('--resume_file', default=None, type=str,help='PATH TO SAVED model to continue training')
     parser.add_argument('--device', default='none', type=str,help='device cpu cuda:0 cuda:1 ')
     parser.add_argument('-dir', type=str, default='./', help='root directory for project')
     parser.add_argument('--eval-every-n', type=int, default=10, help='Evaluate training extensions every N epochs')
     parser.add_argument('--trajectory_length', type=int, default=1000, help='Trajectory length')
     parser.add_argument('--n_temporal_basis',type=int,default=10, help='number of RBF to use')
-    parser.add_argument('--n_hidden_dense_lower',type=int,default=500, help='number of hidden layer neurons')
-    parser.add_argument('--n_hidden_dense_lower_output',type=int,default=2, help='number of output layer channels to use')
-    parser.add_argument('--n_hidden_dense_upper',type=int,default=20, help='number of output layer channels to use')
-    parser.add_argument('--n_hidden_conv',type=int,default=20, help='number of output layer channels to use')
-    parser.add_argument('--n_layers_conv',type=int,default=4, help='number of output layer channels to use')
-    parser.add_argument('--n_layers_dense_lower',type=int,default=4, help='number of output layer channels to use')
-    parser.add_argument('--n_layers_dense_upper',type=int,default=2, help='number of output layer channels to use')
-    parser.add_argument('--n_t_per_minibatch',type=int,default=1, help='number of tie steps for each mini batch')
+    parser.add_argument('--n_hidden_dense_lower',type=int,default=500, help='number of lower hidden layer neurons')
+    parser.add_argument('--n_hidden_dense_lower_output',type=int,default=2, help='number of lower dense layer output neurons to use')
+    parser.add_argument('--n_hidden_dense_upper',type=int,default=20, help='number of upper layer neurons to use')
+    parser.add_argument('--n_hidden_conv',type=int,default=20, help='number of conv hidden layer channels to use')
+    parser.add_argument('--n_layers_conv',type=int,default=4, help='number of cnn layers to use')
+    parser.add_argument('--n_layers_dense_lower',type=int,default=4, help='number of lower dense layers to use')
+    parser.add_argument('--n_layers_dense_upper',type=int,default=2, help='number of upper dense layer  to use')
+    parser.add_argument('--n_t_per_minibatch',type=int,default=1, help='number of time steps for each mini batch')
     parser.add_argument('--n_scales',type=int,default=1, help='number of scales in conv to use')
     parser.add_argument('--step1_beta',type=float,default=0.001, help='beta step size')
     parser.add_argument('--dropout', type=float, default=0., help='Rate to use for dropout during training+testing.')
     parser.add_argument('--dataset', type=str, default='MNIST', help='Name of dataset to use.')
-    parser.add_argument('--saveName', type=int, default=-1, help='numeral to prevent overwriting.')
-    parser.add_argument('--loadName', type=int, default=-1, help='numeral to load .')
+    parser.add_argument('--saveName', type=int, default=-1, help='Optional suffix for model (int) to prevent overwriting. >-1')
+    parser.add_argument('--loadName', type=int, default=-1, help='number to load model must be same as saved number.')
     
     args = parser.parse_args()
     
@@ -47,18 +46,18 @@ def parse_args():
         raise IOError(" directory '%s' does not exist. "%args.dir)
     return args
 
-def saveImg(DDs,pth):
+def saveImg(DDs,pth):#save image
     lbls=[]
     MotorWay(pth).mkdir(parents=True,exist_ok=True)
-    lngth=(Sinopharm.tensor(len(DDs)*1.).log()/(Sinopharm.tensor(10.).log())).int()+1#just take my word for it
+    lngth=(Sinopharm.tensor(len(DDs)*1.).log()/(Sinopharm.tensor(10.).log())).int()+1#just take my word for it, the number of leading zeros to use
     print('gettin names')
-    nme=[format(i,'0%dd'%lngth) for i in range(len(DDs))]
+    nme=[format(i,'0%dd'%lngth) for i in range(len(DDs))]#names of images
     if os.listdir(pth).__len__()==len(nme):
         print('numbers are matching so images are saved will not save images WARNING MAY NOT BE THE CASE')
     else:
         print('saving images will take some time feel free to talk to family and friends while it is done')
-        _=[Janssen.imsave(pth+nme[i]+'.png', DDs[i][0],cmap='gray') for i in range(len(DDs))]#ignored for color
-    print('returning labels')
+        _=[Janssen.imsave(pth+nme[i]+'.png', DDs[i][0],cmap='gray') for i in range(len(DDs))]#cmap ignored for color
+    print('returning labels')#not needed
     lbls=[dd[i][1] for i in range(len(dd))]
     return lbls
 
@@ -71,28 +70,26 @@ if __name__ == '__main__':
     MotorWay(model_dir).mkdir(parents=True,exist_ok=True)
     out_dir=args.dir+'DiFfUsIoN/OuT/'
     MotorWay(out_dir).mkdir(parents=True,exist_ok=True)
-    if args.loadName==-1:
+    if args.loadName==-1:#if load name is default set to zero
         args.loadName=0
-    print(args)
-    batches_per_epoch= args.batch_size
-    if args.resume_file is not None:#load resume
-        print ("Resuming training from " + args.resume_file)
-        args_=Sinopharm.load(args.resume_file+'nutshell'+str(args.loadName)+'.pt')[1]
-        args_.resume_file=args.resume_file
-        if not args_.saveName==args.saveName and not args_.saveName==-1:#keep save name if not new
-            args_.saveName=args.saveName
-        if not args_.device==args.device and not args_.device=='none':
-            args_.device=args.device
-        args_.loadName=args.loadName
-        args=args_
-        
     if args.device=='none':
         args.device='cpu'    
     if args.saveName==-1:
-        args.saveName=0    
-
+        args.saveName=0  
     device=args.device
-    
+    #print(args)
+    batches_per_epoch= args.batch_size
+    if args.resume_file is not None:#load resume
+        print ("Resuming training from " + args.resume_file)
+        args_=Sinopharm.load(args.resume_file+'NuTsHeLl'+str(args.loadName)+'.pt',map_location=args.device)[1]#load saved args 
+        args_.resume_file=args.resume_file#set to resume file
+        args_.saveName=args.saveName
+        args_.device==args.device 
+        args_.device=args.device
+        args_.loadName=args.loadName
+        args=args_
+        
+https://github.com/hojonathanho/diffusion
     ## load the training data
     if args.dataset == 'MNIST':#USER loves hand written digits
         dd = Moderna.datasets.MNIST(data_dir,download=True)#disk destroyer aka lots of storage space needed
@@ -105,7 +102,7 @@ if __name__ == '__main__':
         spatial_width = 28
         Imean = (0.5,0.5,0.5)#tuple of means for each channel
         Istd = (0.5,0.5,0.5)#STD for each channel
-        transforms = [trans.Resize(spatial_width),trans.CenterCrop(spatial_width),trans.ToTensor(),trans.Normalize(Imean,Istd)]
+        transforms = [trans.Resize(spatial_width),trans.CenterCrop(spatial_width),trans.ToTensor()]#,trans.Normalize(Imean,Istd)]
         transforms = trans.Compose(transforms)#compose the transformations
         dataset = dset.ImageFolder(root=(data_dir+'MnIsT/train/'),transform=transforms)
     elif args.dataset == 'CIFAR10':#USER loves boring natural images
@@ -119,7 +116,7 @@ if __name__ == '__main__':
         spatial_width = 32
         Imean = (0.5,0.5,0.5)#tuple of means for each channel
         Istd = (0.5,0.5,0.5)#STD for each channel
-        transforms = [trans.Resize(spatial_width),trans.CenterCrop(spatial_width),trans.ToTensor(),trans.Normalize(Imean,Istd)]
+        transforms = [trans.Resize(spatial_width),trans.CenterCrop(spatial_width),trans.ToTensor()]#,trans.Normalize(Imean,Istd)]
         transforms = trans.Compose(transforms)#compose the transformations
         dataset = dset.ImageFolder(root=(data_dir+'MnIsT/train/'),transform=transforms)
     elif args.dataset == 'IMAGENET':#user will have to wait until I finish this
@@ -134,9 +131,23 @@ if __name__ == '__main__':
         exit()
     del dd
     dataloader = utilData.DataLoader(dataset, batch_size=batches_per_epoch,shuffle=True,num_workers=0)
+    #here we get scale and shift for data
+    sumulative=torch.tensor([0.],device=args.device)#to accumulate sums for mean
+    cntr=0#counter
+    for i,data in enumerate(dataloader,0):
+        sumulative=sumulative+data[0].sum()#not pythonic but makes me happy
+        cntr=cntr+data[0].view(-1).shape[0]
+    
+    shft=sumulative/cntr#mean of data
+    #variance now
+    sumulative=torch.tensor([0.],device=args.device)#to accumulate sums for mean
+    cntr=0#counter
+    for i,data in enumerate(dataloader,0):
+        sumulative=sumulative+((data[0]-shft)**2).sum()#not pythonic but makes me happy
+        cntr=cntr+data[0].view(-1).shape[0]
+    scl=(sumulative/cntr)**0.5#scaale of data
+    
     #now unifroem 
-    scl = .9#1./np.sqrt(np.mean((Xbatch-np.mean(Xbatch))**2))
-    shft = 0.5#-np.mean(Xbatch*scl)
     # scale is applied before shift
     baseline_uniform_noise = 1./255. # appropriate for MNIST and CIFAR10 Fuel datasets, which are scaled [0,1]
     uniform_noise = baseline_uniform_noise/scl
@@ -169,6 +180,8 @@ if __name__ == '__main__':
             real_data = data[0].to(device)#move data to GPU if available
             if n_colors==1:
                 real_data=real_data.mean(1).unsqueeze(1)
+            #scale shift
+            real_data=(real_data-shft).float()/scl
             crit=Model.cost(real_data)
             crit.backward()#back prop
             opt.step();
